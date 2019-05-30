34282b85eeefa25cf135df9ffffc0eab12c889d6
Willyson <37868629+Willyson@users.noreply.github.com>
2019-05-29 23:49:17 -0300

    Aula - 29 05 2019
    Aula referente a criação de procedures.

---
+++ Exercicio.sql
@@ -0,0 +1,39 @@
+/*
+
+1) Sabendo-se que o usuário executará:
+
+a) EXEC sp_exCalc 13, 'FIBO';
+
+b) EXEC sp_exCalc 10, 'FAT';
+
+c) EXEC sp_exCalc 10000, 'IRRF';
+
+d) EXEC sp_exMedia 6, 2, 1.5, 'FMEDIA';
+
+e) EXEC sp_exCalc 10, 'DESC';
+
+f) EXEC sp_exCalc 10, 'AUM';
+
+2) Criar As procedures deverão controlar a DML do banco imóveis
+
+g) Inserir valores nas tabelas (TODAS) do banco imóveis
+
+h) Alterar valores nas tabelas (TODAS) do banco imóveis
+
+i) Excluir valores nas tabelas (TODAS) do banco imóveis
+
+
+PS.: Apresente as procedures em um único arquivo, onde precisa ser calculado:
+
+a) Termos Fibonacci
+
+b) Fatorial
+
+c) Qual valor dado o salário bruto, será descontado para o IRRF
+
+d) Quanto falta para ser aprovado na Fatec Zona Sul
+
+e) Dado a porcentagem calcular o desconto
+f) Dado a porcentagem calcular o aumento
+
+*/
